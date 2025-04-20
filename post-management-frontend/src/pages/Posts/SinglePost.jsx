956e92fbf0a772ad20e750bcd2e39dc4c59bd63e
piyushAgrawal44 <piyushbansal941@gmail.com>
2025-04-20 22:37:29 +0530

    Fix: Added loader while comments are loading

--- post-management-frontend/src/pages/Posts/SinglePost.jsx
+++ post-management-frontend/src/pages/Posts/SinglePost.jsx
@@ -12,6 +12,7 @@ const SinglePost = () => {
     allPostLoading,
     hasMore,
     page,
+    setPage,
     comments,
     commentCount,
     setComments,
@@ -35,7 +36,8 @@ const SinglePost = () => {
       await addCommentsApi(comment, id);
       setComment("");
       setCharCount(0);
-      await getCommentsApi(id, page, 10);
+      setPage(1)
+      await getCommentsApi(id, 1, 10);
     } catch (error) {
       console.error("Failed to add comment:", error);
     } finally {
@@ -69,7 +71,9 @@ const SinglePost = () => {
 
   useEffect(() => {
     if (id) {
-      getCommentsApi(id, page, 10);
+      setComments([])
+      setPage(1)
+      getCommentsApi(id, 1, 10);
     }
 
     // Scroll to top when post loads
